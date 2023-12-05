b5ad56bd29791d356b7c85bb7fa933c4d387bd77
ndundastevn <ndundastevn@gmail.com>
2023-12-05 07:42:32 -0800

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+function getListStudents(){
+	var students = [
+		{
+			firstName: "Guillaume", id: 1, location: "San Francisco"
+		},
+		{
+			firstName: "James", id: 2, location: "Columbia"
+		},
+		{
+			firstName: "Serena", id: 5, location: "San Francisco"
+		}
+	]
+	return students
+}
+
